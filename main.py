198a81116766b4cc8a99307718c10e5f1afccccf
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2021-11-28 15:53:33 +0530

    changes

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from kivy import *
 from kivy.app import App
 from kivy.uix import widget
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
 
 class MyGrid(widget):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
     pass
+
+    def btn(self,instance):
+        print("Nmae:",self.name.text,"email:",self.email.text)
+        self.name.text = ""
+        self.email.text = ""
+
+def btn(instance):
+    print("Run!")
 
 class MyApp(App):
     def build(self):
         return MyGrid()
+
+    def btn(self):y
 
 if __name__=="__main__":
     MyApp().run()
